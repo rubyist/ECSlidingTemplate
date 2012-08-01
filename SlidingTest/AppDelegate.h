d6ff437cd6a7a8bdc5cbf0210c4927accf96fa7a
Scott Barron <scott@elitists.net>
2012-07-31 22:41:32 -0400

    Initial Commit

---
+++ SlidingTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SlidingTest
+//
+//  Created by Scott Barron on 7/31/12.
+//  Copyright (c) 2012 Scott Barron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
